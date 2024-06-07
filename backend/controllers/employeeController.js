5840445456dab55de1e16edc9c6b0dd19d966f5d
Rebaone Faith Matlaba <67489226+faithseasonsdesign@users.noreply.github.com>
2024-06-08 01:36:59 +0200

    updated controllers
    added delete employee functionality to the database and adding employee

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -22,23 +22,77 @@ const retrieveAllEmployees = async (req,res)=>{
 }
 const addNewEmployee = async (req,res)=>{
     const {firstName,lastName,age} = req.body
-    const employee = {firstName,lastName,age}
-    if(firstName && lastName && age){
-        res.status(200).json({message:`added new employee`,employee:employee})
-    }
+    if(!firstName || !lastName || !age){
+        res.json({message:`please provide firstname,lastname and age`})
+        console.log(`please provide the firstname,lastname and age`)
+    }   
     else{
-        res.status(400).json({message: `failed to add employee`})
+        const employeeInfo = {firstName,lastName,age}
+        try{
+            const employeeExist = await employeeModel.findOne({firstName,lastName})
+            if(employeeExist){
+                res.json({message:`employee with firstname ${firstName} and lastname ${lastName} already exist`})
+            }
+            else{
+                try{
+                    const newEmployee = await employeeModel.create(employeeInfo)
+                    if(newEmployee){
+                        res.json({message:`added employee successfuly`,firstName:newEmployee.firstName,lastName:newEmployee.lastName})
+                    }
+                    else{
+                        res.json({message:`failed to add a new employee`})
+                    }
+                }
+                catch(error){
+                    res.json({errorName:`error name : ${error.name}`,errorCode:`error code : ${error.code}`})
+                }
+            }
+        }
+        catch(error){
+            res.json({errorName:`error name : ${error.name}`,errorCode:`error code ${error.code}`})
+        }
     }
 }
+
 const retrieveOneEmployee = async (req,res)=>{
-    res.status(200).json({message:"retrieved one employee"})
-    console.log(`method: ${req.method}`)
-    console.log(`route path: ${req.path}`)
-    console.log(`employee id: ${req.params.employeeId}`)
+    const employeeInfo = req.params
+    try{
+        const employeeExist = await employeeModel.findOne(employeeInfo)
+        if(employeeExist){
+            res.json({message:`employee exist `,firstName:employeeExist.firstName,lastName:employeeExist.lastName})
+        }
+        else{
+            res.json({message:`employee does not exist`})
+        }
+    }
+    catch(error){
+        if(error.name === "ValidationError"){
+            res.json({message:`Validation Error ${error.message}`})
+        }else{
+            res.json({message:`Database Error ${error}`})
+        }
+    }
 }
 const deleteEmployee = async (req,res)=>{
-    res.status(200).json({message:`deleted employee`})
-    console.log(`delete employee with id : ${req.params.employeeId}`)
+    const params = req.params
+    const employeeName = params.employeeId
+    if(employeeName !== ""){
+        try{
+            const deletedEmployee = await employeeModel.findOneAndDelete(employeeName)
+            if(deletedEmployee){
+                res.json({message:`deleted employee successfuly`})
+            }
+            else{
+                res.json({message:`failed to delete employee`})
+            }
+        }
+        catch(error){
+            res.json({message:`database error ${error}`})
+        }
+    }
+    else{
+        res.json({message:`please enter the name of the person you want to delete`})
+    }
 }
 
 const updateEmployee = async (req,res)=>{
